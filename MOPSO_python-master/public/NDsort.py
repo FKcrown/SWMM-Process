5a871fdd528ac48c9645798a0dba0c034a73d7ff
FKcrown <zlbmdzz@163.com>
2023-04-21 21:35:32 +0800

    + 添加了MOPSO改进算法项目文件

---
+++ MOPSO_python-master/public/NDsort.py
@@ -0,0 +1,38 @@
+import numpy as np
+from public import sortrows
+
+
+def NDSort(PopObj, Remain_Num):
+    """
+    该程序永远不会出现重复的解，因为在计算拥挤度的时候，会将重复的解的拥挤度设置为无穷大
+    :param PopObj:
+    :param Remain_Num:
+    :return:
+    """
+    N, M = PopObj.shape
+    FrontNO = np.inf * np.ones((1, N))
+    MaxFNO = 0
+    PopObj, rank = sortrows.sortrows(PopObj)
+
+    while (np.sum(FrontNO < np.inf) < Remain_Num):
+        MaxFNO += 1
+        for i in range(N):
+            if FrontNO[0, i] == np.inf:
+                Dominated = False
+                for j in range(i - 1, -1, -1):
+                    if FrontNO[0, j] == MaxFNO:
+                        m = 2
+                        while (m <= M) and (PopObj[i, m - 1] >= PopObj[j, m - 1]):
+                            m += 1
+                        Dominated = m > M
+                        if Dominated or (M == 2):
+                            break
+                if not Dominated:
+                    FrontNO[0, i] = MaxFNO
+    # temp=np.loadtxt("temp.txt")
+    # print((FrontNO==temp).all())
+    front_temp = np.zeros((1, N))
+    front_temp[0, rank] = FrontNO
+    # FrontNO[0, rank] = FrontNO 不能这么操作，因为 FrontNO值 在 发生改变 ，会影响后续的结果
+
+    return front_temp, MaxFNO
